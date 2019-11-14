189045cce32f33b46663c0669c961a348d14d87c
addod19 <addodaniellarbi@gmail.com>
2019-11-14 16:29:15 +0000

    sessions 1,3

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  
+  resources :sessions, only: [:new, :create, :destroy]
+  match '/signin',  to: 'sessions#new', via: 'get'
+  match '/signout', to: 'sessions#destroy', via: 'delete'
+  get 'sessions/new'
   get 'users/new'
+  
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
