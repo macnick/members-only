0f3c72276f8c983b54730a9c90de9d117e80e6cc
macnick <nick@macexperts.gr>
2019-11-15 12:58:05 +0200

    Session and Signin 6

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,20 @@
 class User < ApplicationRecord
   has_secure_password 
-    
+  before_create :create_remember_token
   
+  # Returns a random token.
+  def User.new_token
+    SecureRandom.urlsafe_base64
+  end
 
+  private
+  
+  def create_remember_token
+    self.remember_token = User.digest(User.new_token)
+  end
+
+  def User.digest(token)
+    Digest::SHA1.hexdigest(token.to_s)
+  end
 end
+ 
