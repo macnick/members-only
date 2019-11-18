20ebb12a343a5076ba10f3f8321a0ca999726643
addod19 <addodaniellarbi@gmail.com>
2019-11-18 13:26:25 +0000

    before_action to restrict access to the #new and #create methods

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,2 +1,9 @@
 class PostsController < ApplicationController
+  before_action :signed_in_user, only: [:new, :create]
+
+  def signed_in_user
+    unless signed_in?
+      redirect_to signin_url
+    end
+  end
 end
