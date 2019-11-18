f13e5806740e5d318d6282bbd2053fba12a11f27
addod19 <addodaniellarbi@gmail.com>
2019-11-18 13:24:48 +0000

    post routes create

--- config/routes.rb
+++ config/routes.rb
@@ -8,5 +8,8 @@ Rails.application.routes.draw do
   get '/signup', to: 'users#new'
   post '/signup', to: 'users#create'
 
+  resources :posts, only: [:new, :create, :index]
+  root 'posts#index'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
