a807a246823017fa0e354d62ba52f4ae47a2c947
addod19 <addodaniellarbi@gmail.com>
2019-11-18 13:27:35 +0000

    line 3

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,10 @@
 class PostsController < ApplicationController
   before_action :signed_in_user, only: [:new, :create]
 
+  def new
+   @post = Post.new
+  end
+
   def signed_in_user
     unless signed_in?
       redirect_to signin_url
