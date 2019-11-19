3dda3f7780f45e662d24dd4c9501947bf170ff0e
stickler-ci <support@stickler-ci.com>
2019-11-19 09:58:42 +0000

    Fixing style errors.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,12 +13,11 @@ class UsersController < ApplicationController
       render 'new'
     end
   end
-  
+
   private
 
-    def user_params
-      params.require(:user).permit(:name, :email, :password,
-                                   :password_confirmation)
-    end
-
+  def user_params
+    params.require(:user).permit(:name, :email, :password,
+                                 :password_confirmation)
+  end
 end
